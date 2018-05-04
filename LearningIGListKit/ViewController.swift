41d8a52fd9bbdf2921781fcf00e73f4c289d1a41
chawatvish <chawatvish.wo@gmail.com>
2018-05-04 09:58:50 +0700

    Initial Commit

---
+++ LearningIGListKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearningIGListKit
+//
+//  Created by Chawatvish Worrapoj on 4/5/2561 BE.
+//  Copyright © 2561 Chawatvish Worrapoj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
